6a0769de213daeb6ddd7da3d2e1b300aaaecff77
Leanne Huq <1590023@gmail.com>
2022-08-11 15:38:20 +0000

    Compeleted perp-javascript-object exercise

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,17 @@
+var person = {
+  firstName: 'Bobby ',
+  lastName: 'Joe',
+  hobby: 'Fishing'
+};
+console.log(person);
+
+const fullName = (person.firstName + person.lastName);
+console.log('The person\'s name is:', fullName);
+
+person.job = ('Nascar Driver');
+console.log('The person\'s current job is:', person.job);
+
+person.previousJob = 'Astronaut';
+console.log('The person\'s previous job was:', person.previousJob);
+
+console.log(person);
